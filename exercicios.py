bc4ff77601aca56f285bc23ab96e8d6d068d4251
Sarah Tumenas <sarah.marques@spread.com.br>
2022-10-28 11:43:03 -0300

    exercicios

---
+++ exercicios.py
@@ -0,0 +1,27 @@
+minha_idade = 26
+idade_namorado = 25
+if(minha_idade == idade_namorado):
+    print('temos idades iguais')
+else:
+    print('temos idades diferentes')
+
+numero1 = 10
+numero2 = 10
+if(numero1 == numero2):
+    print("São números iguais")
+
+idade1 = 10
+idade2 = int("20")
+print(idade1 + idade2)
+
+nome = "Nico"
+sobrenome = "Steppat"
+print(nome + sobrenome)
+
+nome = "Nico"
+sobrenome = "Steppat"
+print(nome, sobrenome)
+
+nome = "Nico"
+sobrenome = "Steppat"
+print(nome, sobrenome, sep="_")
