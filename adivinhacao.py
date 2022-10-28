2a1ad71dd25662737d58db4f76c4612c76b269d1
Sarah Tumenas <sarah.marques@spread.com.br>
2022-10-28 13:53:39 -0300

    while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,26 +3,27 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 13
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu número: ")
+while(rodada <= total_de_tentativas ):
+    print("Tentativas: ", rodada, " de: ", total_de_tentativas)
+    chute = input("Digite o seu número: ")
+    print("Você digitou ", chute)
+    numero_chute = int(chute)
 
-print("Você digitou ", chute)
+    acertou = numero_chute == numero_secreto
+    chute_maior = numero_chute > numero_secreto
+    chute_menor = numero_chute < numero_secreto
 
-numero_chute = int(chute)
-
-acertou = numero_chute == numero_secreto
-
-chute_maior = numero_chute > numero_secreto
-
-chute_menor = numero_chute < numero_secreto
-
-if (acertou):
-    print("Você acertou!!")
-else:
-    if(chute_maior):
-        print("Você errou! Seu chute foi maior que o número secreto")
-    elif(chute_menor):
-        print("Você errou! Seu chute foi menor que o número secreto")
+    if (acertou):
+        print("Você acertou!!")
+    else:
+        if(chute_maior):
+            print("Você errou! Seu chute foi maior que o número secreto")
+        elif(chute_menor):
+            print("Você errou! Seu chute foi menor que o número secreto")
+    rodada = rodada + 1
 
 print("Fim do jogo!")
 
