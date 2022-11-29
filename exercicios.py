a39258ab911f27fc6e6e12c9557591bcf606e447
Sarah Tumenas <sarah.marques@spread.com.br>
2022-11-29 17:25:18 -0300

    conhecendo e trabalhando com listas

--- exercicios.py
+++ exercicios.py
@@ -226,7 +226,7 @@ print("*******************************************************")
 
 colocacao = ['Drible da Emoção', 'Bruxos como Ronaldinho', 'Só golaço', '3x1 não é goleada']
 
-campeao = colocacao[1]
+campeao = colocacao[0]
 
 print(' O grande campeão do torneio é o time ' + campeao)
 #Resultado:  O grande campeão do torneio é o time Bruxos como Ronaldinho
