657c581baed385590381b45e326ad8b8a3046dbb
Sarah Tumenas <sarah.marques@spread.com.br>
2022-11-29 16:34:08 -0300

    game loop

--- forca.py
+++ forca.py
@@ -4,5 +4,14 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
+    print("Fim do Jogo!")
+
 if(__name__ == "__main__"):
     jogar()
