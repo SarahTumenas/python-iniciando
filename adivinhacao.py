4b85e87c1b954d3f3e27c3f10adab6623ed9c020
Sarah Tumenas <sarah.marques@spread.com.br>
2022-11-29 14:38:40 -0300

    adicionando niveis ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,7 +8,20 @@ print("*********************************")
 
 #numero_secreto = round(random.random() * 100) # 0.0 1.0
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 
 
 for rodada in range(1, total_de_tentativas + 1):
