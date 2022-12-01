1ed712173eb32d21752f8cab947868821dcfb723
Sarah Tumenas <sarah.marques@spread.com.br>
2022-12-01 11:40:26 -0300

    criando mais funções

--- exercicios.py
+++ exercicios.py
@@ -346,4 +346,18 @@ imprime_mensagem()
 
 print("*******************************************************")
 
+def soma_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero + segundo_numero)
+
+def subtrai_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero - segundo_numero)
+
+def multiplica_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero * segundo_numero)
+
+def divide_dois_numeros(primeiro_numero, segundo_numero):
+    print(primeiro_numero / segundo_numero)
+
+print("*******************************************************")
+
 
