aa8de6160413dc396714e81c45a6e188ba844a3a
Sarah Tumenas <sarah.marques@spread.com.br>
2022-12-01 09:27:26 -0300

    List Comprehension

--- exercicios.py
+++ exercicios.py
@@ -305,5 +305,11 @@ lista = [fruta.upper() for fruta in frutas]
 print(lista)
 
 print("*******************************************************")
+inteiros = [1,3,4,5,7,8]
+print(inteiros)
+quadrados = [n*n for n in inteiros]
+print(quadrados)
+
+print("*******************************************************")
 
 
